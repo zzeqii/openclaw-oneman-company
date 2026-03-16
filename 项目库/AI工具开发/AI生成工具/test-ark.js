013c0b5faa54510eed4064dbb4432786b2f00908
zzeqii <your-email@example.com>
2026-03-16 20:14:10 +0800

    chore: remove project库 from remote, keep locally via .gitignore

--- 项目库/AI工具开发/AI生成工具/test-ark.js
+++
@@ -1,3 +0,0 @@
-const ark = require('@volcengine/ark');
-console.log('Ark模块导出内容：', Object.keys(ark));
-console.log('默认导出：', ark.default ? Object.keys(ark.default) : '无默认导出');
--- 项目库/AI音乐自媒体/工具/SeedDance/test-sdk.js
+++
@@ -1,4 +0,0 @@
-const Ark = require('@volcengine/ark');
-console.log('SDK导出内容：', Object.keys(Ark));
-console.log('default导出：', Ark.default);
-console.log('是否有createClient方法：', typeof Ark.createClient);
